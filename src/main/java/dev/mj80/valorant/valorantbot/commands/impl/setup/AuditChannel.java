36a44516fd8b1de6a1e3c66cb16a0d7045ed2662
TotallyTrying <124003685+TotallyTrying@users.noreply.github.com>
2023-04-09 18:06:24 -0500

    added auditchannel command

---
+++ src/main/java/dev/mj80/valorant/valorantbot/commands/impl/setup/AuditChannel.java
@@ -0,0 +1,43 @@
+package dev.mj80.valorant.valorantbot.commands.impl.setup;
+
+import dev.mj80.valorant.valorantbot.commands.DiscordCommand;
+import dev.mj80.valorant.valorantbot.utils.BotUtils;
+import dev.mj80.valorant.valorantbot.utils.CoreUtils;
+import lombok.Getter;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AuditChannel extends DiscordCommand {
+
+    @Getter
+    private final SlashCommandData commandData =
+            Commands.slash("auditchannel", "Used to declare the channel where the audit log will be posted")
+                    .addOption(OptionType.CHANNEL, "channel", "The audit log channel", true);
+
+    @Override
+    public void run(SlashCommandInteractionEvent event) {
+        event.deferReply().setEphemeral(true).queue();
+        long role = event.getOption("channel").getAsLong();
+
+        String settingsFile = CoreUtils.readFile("settings.txt");
+        List<String> settings = new ArrayList<>(Arrays.asList(settingsFile.split("\n")));
+
+        if (BotUtils.checkRole(event.getMember(), "admin")) {
+            if (CoreUtils.hasSetting(settings, "auditLogChannel")) {
+                String setting = String.valueOf(settings.stream().filter(line -> line.startsWith("auditLogChannel")).findFirst());
+                settings.set(settings.indexOf(setting), "auditLogChannel = " + role);
+            } else {
+                settings.add("auditLogChannel = " + role);
+            }
+            CoreUtils.writeFileFromList("settings.txt", settings);
+        }
+
+        event.getHook().deleteOriginal().queue();
+    }
+}
--- src/main/java/dev/mj80/valorant/valorantbot/managers/CommandManager.java
+++ src/main/java/dev/mj80/valorant/valorantbot/managers/CommandManager.java
@@ -5,6 +5,7 @@ import dev.mj80.valorant.valorantbot.commands.impl.Link;
 import dev.mj80.valorant.valorantbot.commands.impl.Stats;
 import dev.mj80.valorant.valorantbot.commands.impl.help.Help;
 import dev.mj80.valorant.valorantbot.commands.impl.setup.AdminRole;
+import dev.mj80.valorant.valorantbot.commands.impl.setup.AuditChannel;
 import dev.mj80.valorant.valorantbot.commands.impl.setup.ModRole;
 import lombok.Getter;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
@@ -21,6 +22,7 @@ public class CommandManager {
         commands.add(new Help());
         commands.add(new AdminRole());
         commands.add(new ModRole());
+        commands.add(new AuditChannel());
         
         commands.forEach(command -> commandsData.add(command.getCommandData()));
     }
